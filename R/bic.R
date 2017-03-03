41609fea6af242dcf5fe9e0e79187681f68adbf4
Evan Friedland <efriedland@users.noreply.github.com>
2017-03-02 21:21:05 -0500

    Update bic.R
    length(model[["coeff"]]) would return zero, while length(variable.names(fit$model)) is accurate.

--- R/bic.R
+++ R/bic.R
@@ -1,5 +1,5 @@
 bic <-
 function(model){
-  n <- df.residual(model) + length(model[["coeff"]])
-  log(sum(resid(model) ^ 2) / n) + (length(model[["coeff"]])) * log(n) / n
+  n <- df.residual(model) + length(variable.names(fit$model))
+  log(sum(resid(model) ^ 2) / n) + length(variable.names(fit$model)) * log(n) / n
 }
