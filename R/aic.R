a8f018f3af4c54c80d003555589348ebaac77fe4
Evan Friedland <efriedland@users.noreply.github.com>
2017-03-02 21:38:43 -0500

    Update aic.R

--- R/aic.R
+++ R/aic.R
@@ -1,5 +1,5 @@
 aic <-
 function(model){
-  n <- df.residual(model) + length(variable.names(fit$model))
-  log(sum(resid(model) ^ 2) / n) + (length(variable.names(fit$model)) / n) * 2
+  n <- df.residual(model) + length(variable.names(model))
+  log(sum(resid(model) ^ 2) / n) + (length(variable.names(model)) / n) * 2
 }
