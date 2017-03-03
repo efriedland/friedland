970b8d77d9af4d057475d2cde2a96f33c54d3bf3
Evan Friedland <efriedland@users.noreply.github.com>
2017-03-02 21:39:14 -0500

    Update bic.R

--- R/bic.R
+++ R/bic.R
@@ -1,5 +1,5 @@
 bic <-
 function(model){
-  n <- df.residual(model) + length(variable.names(fit$model))
-  log(sum(resid(model) ^ 2) / n) + length(variable.names(fit$model)) * log(n) / n
+  n <- df.residual(model) + length(variable.names(model))
+  log(sum(resid(model) ^ 2) / n) + length(variable.names(model)) * log(n) / n
 }
