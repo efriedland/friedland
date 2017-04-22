636359a9208a814b275e814421727c055fa506fd
Evan Friedland <efriedland@users.noreply.github.com>
2017-04-22 14:06:15 -0400

    Huge Revamp
    Added the ability to fix the number of lags/leads, the ability to do robusterrors is now up to the user, and the output is condensed into the $model. Had to replace the model$coefficients with a summary-style coefficients matrix.

--- R/buildDOLS.R
+++ R/buildDOLS.R
@@ -1,38 +1,42 @@
-buildDOLS <-
-function(coint.formula, data) {
+buildDOLS <- 
+function (coint.formula, data, robusterrors = FALSE, fixedk = NULL){
   stopifnot(is.ts(data))
+  stopifnot(is.null(fixedk)|is.numeric(fixedk))
+  output <- list(data.names = colnames(data))
   ff <- coint.formula
   input.vars <- model.frame(formula = ff, data = data)
-  RHS <- model.matrix(object = ff, data = input.vars) # right hand side
-  maxLL <- floor(dim(input.vars)[1]^(1/3)/2) # choosing max lag/lead length
-  # maxLL for max Leads/Lags
-  # creating the call formula
-  ff.maxLL.char <- paste0(colnames(input.vars)[1], " ~ ", # Y variable
-                          ifelse("(Intercept)" %in% colnames(RHS)," 1 + ", "-1 + "), # Int
-                          paste(colnames(input.vars[-1]), collapse = " + "), # X variables
-                          " + ", 
-                          paste0("L(diff(", 
-                                 colnames(input.vars)[-1],
-                                 "),-maxLL:maxLL)", collapse = " + ")
-                          ) # Lagged Differences of X variables
-  # Saving the DOLS at the max leads & lags so we can fix the start and end date of the selection process
-  DOLS.maxLL <- dynlm(as.formula(ff.maxLL.char), data = data)
-  # replace "maxLL" with "k"
-  ff.k.char <- gsub("maxLL","k",ff.maxLL.char)
-  # Retrieve the BIC for each model 
-  DOLS.bic <- sapply(1:maxLL, 
-                     function(k) bic(dynlm(as.formula(ff.k.char), 
-                                           data = data, 
-                                           start = start(DOLS.maxLL), 
-                                           end = end(DOLS.maxLL))))
-  selection <- cbind(`# of lags/leads` = 1:maxLL, SBC = DOLS.bic)
-  k <- which.min(DOLS.bic) 
-  DOLS.k <- dynlm(formula(ff.k.char), data = data) # best and final model
-  list(data.names = colnames(data), # colnames of data to be used for external functions
-       call = ff.k.char, # the formula called in the final model
-       selection = selection, # BIC results
-       leadslags = k, # the selected number of leads/lags
-       model = DOLS.k, # the final model
-       robusterrors = lmtest::coeftest(DOLS.k, vcov = sandwich::NeweyWest(DOLS.k, lag = k)) # Implementing the Newey & West (1987, 1994) heteroskedasticity and autocorrelation consistent (HAC) covariance matrix estimators
-       )
+  RHS <- model.matrix(object = ff, data = input.vars) # just to create the intercept value...if it's there...
+  ff.LHS <- colnames(input.vars)[1]
+  ff.RHS <- paste(c(ifelse("(Intercept)" %in% colnames(RHS), "1", "-1"), # constant
+                    colnames(input.vars[-1]), # input variables
+                    paste0("L(diff(", colnames(input.vars)[-1], "),-maxLL:maxLL)")),
+                  collapse=" + ")
+  ff.maxLL <- paste(ff.LHS, "~", ff.RHS)
+  maxLL <- ifelse(is.null(fixedk), floor(dim(input.vars)[1]^(1/3)/2), fixedk) 
+  DOLS.maxLL <- dynlm(formula(ff.maxLL), data = data)
+  # if user did not enter a fixed number of lags / leads
+  if(is.null(fixedk)){
+    DOLS.bic <- sapply(1:maxLL, function(maxLL) bic(dynlm(formula(ff.maxLL), data = data, start = start(DOLS.maxLL), end = end(DOLS.maxLL))))
+    output$selection <- cbind(`# of lags/leads (k)` = 1:maxLL, SBC = DOLS.bic)
+    maxLL.bic <- which.min(DOLS.bic)
+    # no need to reestimate, speed optimized here...
+    if(maxLL != maxLL.bic){
+      output$k <- which.min(maxLL.bic)
+      DOLS.k <- dynlm(formula(gsub("maxLL", "maxLL.bic", ff.maxLL)), data = data)
+    } else {
+      output$k <- maxLL.bic
+      DOLS.k <- DOLS.maxLL
+    }
+  } else {
+    output$k <- maxLL
+    DOLS.k <- DOLS.maxLL
+  }
+  output$model <- DOLS.k
+  output$model$call <- as.formula(gsub("maxLL", "k", ff.maxLL))
+  if(robusterrors){
+    output$model$coefficients <- unclass(lmtest::coeftest(DOLS.k, vcov = sandwich::NeweyWest(DOLS.k, lag = output$k))) 
+  } else {
+    output$model$coefficients <- summary(output$model)$coefficients
+  }
+  output
 }
