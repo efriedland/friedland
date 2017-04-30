2b7cb42a5ebd56d980557868bba456c79b04a65a
Evan Friedland <efriedland@users.noreply.github.com>
2017-04-30 02:05:37 -0400

    Added auto lags (k) assignment

--- R/UnitRoot.R
+++ R/UnitRoot.R
@@ -6,10 +6,8 @@ function(variable,           # variable
          cointvariables = 1, # number of variables in relationship
          startfrom = 0){
   if(is.null(dim(variable))) {
-    if(missing(k)) {
-      cat("Pick a maximum lag length\n")
-      cat("Possibly Run: \nfloor(length(",deparse(substitute(variable)),")^(1/3))\n")
-      stop()
+    k <- ifelse(missing(k), floor(length(variable)^(1/3)), k) # ad hoc method of obtaining max lags to use
+    if(missing(k)) warning("No lags supplied (k). Used ", n, "lags by default: floor(length(",deparse(substitute(variable)),")^(1/3))\n")
     }
     M <- matrix(0, ncol=5, nrow = k)
     colnames(M) <- c("Lags", "AIC", "ADF", "#Obs", "Start Date")  
@@ -99,11 +97,8 @@ function(variable,           # variable
       }
     }
   } else {
-    if(missing(k)) {
-      cat("Pick a maximum lag length\n")
-      cat("Possibly Run: \nfloor(ncol(",deparse(substitute(variable)),")^(1/3))\n")
-      stop()
-    }
+    k <- ifelse(missing(k), floor(nrow(variable)^(1/3)), k) # ad hoc method of obtaining max lags to use
+    if(missing(k)) warning("No lags supplied (k). Used ", n, "lags by default: floor(nrow(",deparse(substitute(variable)),")^(1/3))\n")
     t(sapply(lapply(variable, UnitRoot, k = k, drift = drift, trend = trend, 
                     cointvariables = cointvariables, startfrom = startfrom), 
              function(x) x[2:7]))
