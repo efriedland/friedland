31188389e0881b8c79d76f0ad1b6bbe96b1f5807
Evan Friedland <efriedland@users.noreply.github.com>
2017-05-11 10:42:19 -0400

    Streamlined output$ creation and added robusterror

--- R/buildDOLS.R
+++ R/buildDOLS.R
@@ -1,45 +1,41 @@
 buildDOLS <- 
-function (coint.formula, data, robusterrors = FALSE, fixedk = NULL){
-  stopifnot(is.ts(data))
-  stopifnot(is.null(fixedk)|is.numeric(fixedk))
-  output <- list()
-  ff <- coint.formula
-  all.names <- attr(attr(terms(ff), "factors"), "dimnames")
-  y.names <- all.names[[1]][!(all.names[[1]] %in% all.names[[2]])]
-  x.names <- all.names[[2]][all.names[[2]] %in% colnames(data)]
-  ff.LHS <- y.names
-  ff.RHS <- paste(c(ifelse(attr(terms(ff), "intercept") == 1, "1", "-1"), # constant
-                    x.names, # input variables
-                    paste0("L(diff(", x.names, "),-maxLL:maxLL)")),
-                  collapse=" + ")
-  ff.maxLL <- paste(ff.LHS, "~", ff.RHS)
-  maxLL <- ifelse(is.null(fixedk), floor(dim(data)[1]^(1/3)/2), fixedk) 
-  DOLS.maxLL <- dynlm(formula(ff.maxLL), data = data)
-  # if user did not enter a fixed number of lags / leads
-  if(is.null(fixedk)){
-    DOLS.bic <- sapply(1:maxLL, function(maxLL) bic(dynlm(formula(ff.maxLL), data = data, start = start(DOLS.maxLL), end = end(DOLS.maxLL))))
-    output$selection <- cbind(`# of lags/leads (k)` = 1:maxLL, SBC = DOLS.bic)
-    maxLL.bic <- which.min(DOLS.bic)
-    # no need to reestimate, speed optimized here...
-    if(maxLL != maxLL.bic){
-      output$k <- maxLL.bic
-      DOLS.k <- dynlm(formula(gsub("maxLL", "maxLL.bic", ff.maxLL)), data = data)
-      DOLS.k$call <- formula(gsub("maxLL", "maxLL.bic", ff.maxLL))
-    } else { # maxLL == maxLL.bic
-      output$k <- maxLL
-      DOLS.k <- DOLS.maxLL # no need to re-estimate
-      DOLS.k$call <- formula(ff.maxLL)
+  function (coint.formula, data, fixedk = NULL, robusterrors = FALSE){
+    stopifnot(is.ts(data))
+    stopifnot(is.null(fixedk)|is.numeric(fixedk))
+    ff <- coint.formula
+    all.names <- attr(attr(terms(ff), "factors"), "dimnames")
+    y.names <- all.names[[1]][!(all.names[[1]] %in% all.names[[2]])]
+    x.names <- all.names[[2]][all.names[[2]] %in% colnames(data)]
+    ff.LHS <- y.names
+    ff.RHS <- paste(c(ifelse(attr(terms(ff), "intercept") == 1, "1", "-1"), # constant
+                      x.names, # input variables
+                      paste0("L(diff(", x.names, "),-k:k)")), # lagged differences
+                    collapse=" + ")
+    ff.k <- paste(ff.LHS, "~", ff.RHS)
+    k <- ifelse(is.null(fixedk), floor(dim(data)[1]^(1/3)), fixedk)
+    DOLS.k <- dynlm(formula(ff.k), data = data) # maxlag or fixedk value 
+    output <- list()
+    # Lag/Lead Selection method if used did not enter a fixed number
+    if(is.null(fixedk)){
+      k.bic <- sapply(1:k, function(k) bic(dynlm(formula(ff.k), data = data, 
+                                                 start = start(DOLS.k), 
+                                                 end = end(DOLS.k))))
+      output$selection <- cbind(`# of lags/leads (k)` = 1:k, 
+                                SBC = k.bic, 
+                                `#Obs` = DOLS.k$df + length(DOLS.k$coeff), 
+                                `StartDate` = start(DOLS.k)[1], 
+                                `EndDate` = end(DOLS.k)[1])
+      if(k != which.min(k.bic)){
+        k <- which.min(k.bic)
+        DOLS.k <- dynlm(formula(ff.k), data = data)
+      } 
     }
-  } else { # if user did enter a fixed number
-    output$k <- maxLL
-    DOLS.k <- DOLS.maxLL
-    DOLS.k$call <- formula(ff.maxLL)
+    output$k <- k
+    DOLS.k$call <- paste0("dynlm(formula = ",ff.k,", data = ",substitute(data),")")
+    output$model <- DOLS.k
+    
+    if(robusterrors){
+      output$robusterrors <- lmtest::coeftest(DOLS.k, vcov = sandwich::NeweyWest(DOLS.k, lag = output$k))
+    } 
+    output
   }
-  output$model <- DOLS.k
-  if(robusterrors){
-    output$model$coefficients <- unclass(lmtest::coeftest(DOLS.k, vcov = sandwich::NeweyWest(DOLS.k, lag = output$k))) 
-  } else {
-    # output$model$coefficients <- summary(output$model)$coefficients # was stupid to do
-  }
-  output
-}
