a51fc6aecde1ac93718fb65b20e1dabf070f7177
Evan Friedland <efriedland@users.noreply.github.com>
2017-05-16 16:04:56 -0400

    removed AvgSignif and added drop = F to output

--- R/lazybuildDOLS.R
+++ R/lazybuildDOLS.R
@@ -1,8 +1,9 @@
 lazybuildDOLS <- 
 function(results.coint, dat){
+  ff <- levels(results.coint[,"Formula"])
   mod.LagsnLeads <- mod.significance <- mod.adj.R2 <- mod.bic <- mod.Start <- mod.End <- mod.Nobs <- list()
-  for(i in 1:length(levels(results.coint[,"Formula"]))){
-    x <- levels(results.coint[,"Formula"])[i]
+  for(i in 1:length(ff)){
+    x <- ff[i]
     dols <- buildDOLS(as.formula(x), data = dat)
     mod.LagsnLeads[[i]] <- dols$k
     mod.significance[[i]] <- tryCatch({
@@ -16,8 +17,11 @@ function(results.coint, dat){
   }
   Yvar <- strsplit(as.character(results.coint[1,1]), " ~ ")[[1]][1]
   nVar <- colnames(dat)[-which(colnames(dat) == Yvar)]
-  Compare_Cols <- c("(Intercept)", nVar, "adj.R2","Lags/Leads", "Start", "End", "Nobs", "bic", "AvgSignif")
-  Compare_DOLS <- matrix(NA, nrow = length(mod.significance), ncol = length(Compare_Cols), dimnames = list(names(mod.significance), Compare_Cols))
+  Compare_Cols <- c("(Intercept)", nVar, "adj.R2","Lags/Leads", "Start", "End", "Nobs", "bic")
+  Compare_DOLS <- matrix(NA, nrow = length(mod.significance), 
+                         ncol = length(Compare_Cols), 
+                         dimnames = list(names(mod.significance), 
+                                         Compare_Cols))
   InCol <- lapply(mod.significance, function(x) match(names(x), Compare_Cols)) # where to put the significance values
   for(i in 1:length(mod.significance)){ Compare_DOLS[i, InCol[[i]]] <- round(mod.significance[[i]],3) }
   Compare_DOLS[,"adj.R2"] <- unlist(mod.adj.R2)
@@ -26,7 +30,6 @@ function(results.coint, dat){
   Compare_DOLS[,"End"] <- unlist(mod.End)
   Compare_DOLS[,"Nobs"] <- unlist(mod.Nobs)
   Compare_DOLS[,"bic"] <- unlist(mod.bic)
-  Compare_DOLS[,"AvgSignif"] <- rowSums(Compare_DOLS[,3:(1+length(nVar))], na.rm = T) /unlist(lapply(mod.significance, function(x) length(x)-1))
-  rownames(Compare_DOLS) <- levels(results.coint[,"Formula"])
-  as.data.frame(Compare_DOLS[order(Compare_DOLS[,"AvgSignif"]),]) # Comparison across all cointegrating buildDOLS results
+  rownames(Compare_DOLS) <- ff
+  as.data.frame(Compare_DOLS[order(Compare_DOLS[,"bic"]),,drop = F]) # Comparison across all cointegrating buildDOLS results
 }
